7227ab8cfec4b72abeab52d71266407b8e9ac9de
kamada <noinci1209@gmail.com>
2021-01-09 23:28:36 +0900

    デプロイ

--- src/todo/Item.js
+++ src/todo/Item.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Item = ({ content, id, deleteTodo  }) => {
+const Item = ({ content, id, deleteTodo }) => {
     const [isDone, setIsDone] = useState(false)
 
     const handleDelete = () => {
@@ -13,7 +13,7 @@ const Item = ({ content, id, deleteTodo  }) => {
                 setIsDone(!isDone)
             }} />
             <span style={
-                {textDecoration: isDone ? 'line-through' : 'none'}
+                { textDecoration: isDone ? 'line-through' : 'none' }
             }>{content}</span>
             <button onClick={handleDelete}>削除</button>
         </li>
